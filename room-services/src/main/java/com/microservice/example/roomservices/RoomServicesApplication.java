cb62b0c5988f5ab5b8f28a3f994c7dcff3641013
nag_pradipta@yahoo.com <Kolkata@1234567890>
2020-04-05 11:31:10 +0530

    Eureka set up (Client + Server)

--- room-services/src/main/java/com/microservice/example/roomservices/RoomServicesApplication.java
+++ room-services/src/main/java/com/microservice/example/roomservices/RoomServicesApplication.java
@@ -2,6 +2,7 @@ package com.microservice.example.roomservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,19 +15,20 @@ import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class RoomServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoomServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoomServicesApplication.class, args);
+    }
 
-	@Bean
-	public Docket api(){
-		return new Docket(DocumentationType.SWAGGER_2).groupName("Room").select()
-				.apis(RequestHandlerSelectors.basePackage("com.microservice.example.roomservices"))
-				.paths(any()).build().apiInfo(new ApiInfo("Room Services",
-						"A set of services to provide data access to rooms", "1.0.0", null,
-						new Contact("Frank Moley", "https://twitter.com/fpmoles", null),null, null));
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Room").select()
+                .apis(RequestHandlerSelectors.basePackage("com.microservice.example.roomservices"))
+                .paths(any()).build().apiInfo(new ApiInfo("Room Services",
+                        "A set of services to provide data access to rooms", "1.0.0", null,
+                        new Contact("Frank Moley", "https://twitter.com/fpmoles", null), null, null));
+    }
 
 }

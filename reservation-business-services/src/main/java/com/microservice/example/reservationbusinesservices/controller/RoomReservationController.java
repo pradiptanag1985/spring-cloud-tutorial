7567c1c46a419f9f4e6b235d97b65736fcc0ee9c
nag_pradipta@yahoo.com <Kolkata@1234567890>
2020-04-05 12:16:51 +0530

    Adding the module for Rest Client using Ribbon Load Balancing

---
+++ reservation-business-services/src/main/java/com/microservice/example/reservationbusinesservices/controller/RoomReservationController.java
@@ -0,0 +1,30 @@
+package com.microservice.example.reservationbusinesservices.controller;
+
+import com.microservice.example.reservationbusinesservices.domain.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class RoomReservationController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/rooms", method = RequestMethod.GET)
+    public List<Room> getAllRooms() {
+        ResponseEntity<List<Room>> roomsResponse = this.restTemplate.exchange(
+                "http://room-services/rooms", HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Room>>() {
+                });
+        return roomsResponse.getBody();
+    }
+}
---
+++ reservation-business-services/src/main/java/com/microservice/example/reservationbusinesservices/domain/Room.java
@@ -0,0 +1,51 @@
+package com.microservice.example.reservationbusinesservices.domain;
+
+public class Room {
+
+    private long id;
+    private String roomName;
+    private String roomNumber;
+    private String bedInfo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getBedInfo() {
+        return bedInfo;
+    }
+
+    public void setBedInfo(String bedInfo) {
+        this.bedInfo = bedInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomName='" + roomName + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                ", bedInfo='" + bedInfo + '\'' +
+                '}';
+    }
+}
